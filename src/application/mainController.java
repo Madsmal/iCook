1c4e158297539712ec6b85c26f03eb1ab1cec458
Mads Malmskov <Madsmalmskov@hotmail.com>
2018-02-27 00:08:43 +0100

    Added scene builder

---
+++ src/application/mainController.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class mainController {
+
+	public void onClick(ActionEvent event) {
+		System.out.println("Exit");
+		System.exit(0);
+	}
+	
+}
