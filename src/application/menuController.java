8b2cdb7625ed8a0e98836f04e74deb9312a8fe3c
Madsmal <madsmalmskov@hotmail.com>
2018-02-28 14:27:28 +0100

    system exit 1

--- src/application/menuController.java
+++ src/application/menuController.java
@@ -1,13 +1,12 @@
 package application;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 
 public class menuController {
 
 	public void onExit(ActionEvent event) {
 		System.out.println("Exit");
-		Platform.exit();
+		System.exit(1);
 	}
 	
 }
