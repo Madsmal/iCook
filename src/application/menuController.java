e270808ed4185b0b5c161ba684923f519bbd001d
Madsmal <madsmalmskov@hotmail.com>
2018-03-07 14:46:31 +0100

    Added GUI to cookingView

--- src/application/menuController.java
+++ src/application/menuController.java
@@ -1,24 +1,30 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class menuController {
 	
-public void onRecipes(ActionEvent event) {
+	Stage primaryStage;
+	
+public void onRecipes(ActionEvent event) throws Exception {
 	//Parent recipesPath = FXMLLoader.load(getClass().getResource("/application/recipesView.fxml"));
 	//Scene recipesScene = new Scene(recipesPath);
 	//primaryStage.getScene().setRoot(recipesScene);
 	
 	//RecipesController recipesController = new RecipesController(model);
 	//recipesScene.show();
+	
+	
 }
 
 
-	
 public void onEditor(ActionEvent event) {
 	
 }
