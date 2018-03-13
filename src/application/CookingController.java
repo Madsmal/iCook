bf407a8cd5fe36f16e635a66b8423fe4606ca944
Mads Malmskov <Madsmalmskov@hotmail.com>
2018-03-13 19:32:09 +0100

    Added action for home + GUI

--- src/application/CookingController.java
+++ src/application/CookingController.java
@@ -3,13 +3,15 @@ package application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 
+
 public class CookingController {
 
-	
-	
+
 	public void onHome(ActionEvent event) {
 		System.out.println("Exit");
 		Platform.exit();
 	}
+
+	
 
 }
--- src/application/RecipesController.java
+++ src/application/RecipesController.java
@@ -1,12 +1,20 @@
 package application;
 
-import javafx.application.Platform;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class RecipesController {
 
-public void onHome(ActionEvent event) {
-		
+public void onHome(ActionEvent event) throws Exception {
+	
+	Parent home = FXMLLoader.load(getClass().getResource("/application/MenuView.fxml"));
+	Scene Home = new Scene(home);
+	Model.primaryStage.setScene(Home);		
+	Model.primaryStage.show();
+	
 	}
 	
 }
