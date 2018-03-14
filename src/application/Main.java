e9366c40cae0b399762e0bcf411f547f061d6a12
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2018-03-14 13:19:29 +0100

    disabled Model.jar instantiation

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,7 +9,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Model model = new Model(primaryStage);
+		//Model model = new Model(primaryStage);
+		Model.primaryStage = primaryStage;
 		
 		Parent root = FXMLLoader.load(getClass().getResource("/application/MenuView.fxml"));
 		Scene scene = new Scene(root);
--- src/application/Model.java
+++ src/application/Model.java
@@ -3,10 +3,10 @@ package application;
 import javafx.stage.Stage;
 
 public class Model {
-	
+	/*
 	public Model (Stage stage) {
 		this.primaryStage = stage;
 	}
-	
+	*/
 	static Stage primaryStage;
 }
