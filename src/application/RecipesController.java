d618ad3f513f3e5ce1358fc27c06e7cf38324ad9
Madsmal <madsmalmskov@hotmail.com>
2018-03-21 15:28:12 +0100

    Added listView

--- src/application/RecipesController.java
+++ src/application/RecipesController.java
@@ -1,27 +1,46 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 
-public class RecipesController {
-
-public void onHome(ActionEvent event) throws Exception {
+public class RecipesController implements Initializable {
 	
-	Parent home = FXMLLoader.load(getClass().getResource("/application/MenuView.fxml"));
-	Scene Home = new Scene(home);
-	Model.primaryStage.setScene(Home);		
-	Model.primaryStage.show();
+	public static String[] recipeLibraryList;
+	
+	@FXML 
+	private ListView<String> listView;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		ObservableList<String> data = FXCollections.observableArrayList("Test1","Test2");
+	//	ObservableList<String> data = recipeLibraryList
+		listView.setItems(data);
 	}
 
-public void onStart(ActionEvent event) throws Exception {
-	
-	Parent start = FXMLLoader.load(getClass().getResource("/application/CookingView.fxml"));
-	Scene Start = new Scene(start);
-	Model.primaryStage.setScene(Start);		
-	Model.primaryStage.show();
+	public void onHome(ActionEvent event) throws Exception {
+
+		Parent home = FXMLLoader.load(getClass().getResource("/application/MenuView.fxml"));
+		Scene Home = new Scene(home);
+		Model.primaryStage.setScene(Home);		
+		Model.primaryStage.show();
 	}
-	
+
+	public void onStart(ActionEvent event) throws Exception {
+
+		Parent start = FXMLLoader.load(getClass().getResource("/application/CookingView.fxml"));
+		Scene Start = new Scene(start);
+		Model.primaryStage.setScene(Start);		
+		Model.primaryStage.show();
+	}
+
 }
