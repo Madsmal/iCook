4b089fe481c630f957bc876d5ace30eb7f7816f5
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2018-04-11 16:56:56 +0200

    prev and next button works with currentTask variable

--- src/application/CookingController.java
+++ src/application/CookingController.java
@@ -9,27 +9,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 
 public class CookingController implements Initializable {
 	
 	// Defining variables
-	int[] taskSequence;
+	int[] taskSequence = new int[5];//the 5 length is temporary, when final no init is needed 
 	int currentTask = -1;
 	
 	
 	
 	// Defining FXML elements
+	@FXML Button previous;
 	@FXML Label currentTime;
 	@FXML Label task;
 	
 	public void initialize(URL url, ResourceBundle rb) {
 		
+		
 		//taskSequence = calculateTaskSequence();
 	
 		// Default FXML elements values
 		//task.setText("Get all your ingredients ready:\n"); //TODO: insert ingredients
+		
 		
 	}
 	
@@ -45,12 +49,19 @@ public class CookingController implements Initializable {
 	}
 	
 	public void onNext(ActionEvent event) throws Exception {
-		currentTask++;
+		if (currentTask != taskSequence.length-1) {
+			currentTask++;
+		}
+		System.out.println("currentTask = "+currentTask);
 		//task.setText(Model.recipe.getIngredients().getIngredient().get(i).getTaskString());
 	}
 	
 	public void onPrevious(ActionEvent event) throws Exception {
-		currentTask--;
+		if (currentTask != -1) {
+			currentTask--;
+		}
+		System.out.println("currentTask = "+currentTask);
+		//if currentTask == -1 then show ingredients else:
 		//task.setText(Model.recipe.getIngredients().getIngredient().get(i).getTaskString());
 	}
 	
@@ -68,4 +79,5 @@ public class CookingController implements Initializable {
 		return taskSequence;
 	}
 	*/
+	
 }
