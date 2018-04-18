b87266ab5fb64768af1ff7f8facaca50706094d1
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2018-04-18 16:58:11 +0200

    tasks show with prev and next button

--- src/application/CookingController.java
+++ src/application/CookingController.java
@@ -11,29 +11,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 
 
 public class CookingController implements Initializable {
 	
 	// Defining variables
 	int[] taskSequence = new int[5];//the 5 length is temporary, when final no init is needed 
-	int currentTask = -1;
+	int currentTask = 0;
+	int timePassed = 0;
 	
 	
 	
 	// Defining FXML elements
+	@FXML ProgressBar pb;
 	@FXML Button previous;
 	@FXML Label currentTime;
 	@FXML Label task;
 	
 	public void initialize(URL url, ResourceBundle rb) {
 		
+		//TEMPORARY START
+		taskSequence[0] = 1;
+		taskSequence[1] = 2;
+		taskSequence[2] = 3;
+		taskSequence[3] = 4;
+		taskSequence[4] = 5;
+		//TEMPORARY END
+		
 		
 		//taskSequence = calculateTaskSequence();
+		
 	
 		// Default FXML elements values
-		//task.setText("Get all your ingredients ready:\n"); //TODO: insert ingredients
-		
+		task.setText(Model.recipe.getTasks().getTask().get(taskSequence[currentTask]).getTaskString());
 		
 	}
 	
@@ -51,18 +62,25 @@ public class CookingController implements Initializable {
 	public void onNext(ActionEvent event) throws Exception {
 		if (currentTask != taskSequence.length-1) {
 			currentTask++;
+			//timePassed = timePassed +
+			//pb.setProgress(7/(currentTask+2)); todo: update based on tasktime and totaltime
 		}
-		System.out.println("currentTask = "+currentTask);
-		//task.setText(Model.recipe.getIngredients().getIngredient().get(i).getTaskString());
+		System.out.println("currentTask = "+currentTask+" ; timePassed = "+timePassed);
+		task.setText(Model.recipe.getTasks().getTask().get(taskSequence[currentTask]).getTaskString());
 	}
 	
 	public void onPrevious(ActionEvent event) throws Exception {
-		if (currentTask != -1) {
+		if (currentTask != 0) {
 			currentTask--;
-		}
-		System.out.println("currentTask = "+currentTask);
-		//if currentTask == -1 then show ingredients else:
-		//task.setText(Model.recipe.getIngredients().getIngredient().get(i).getTaskString());
+			//timePassed = timePassed - task.setText(Model.recipe.getTasks().getTask().get(taskSequence[currentTask-1]).getTime());
+		} 
+		
+		
+		task.setText(Model.recipe.getTasks().getTask().get(taskSequence[currentTask]).getTaskString());
+		
+		System.out.println("currentTask = "+currentTask+" ; timePassed = "+timePassed);
+		
+		//pb.setProgress(7/(currentTask+2)); todo: update based on tasktime and totaltime
 	}
 	
 	public void onPause(ActionEvent event) throws Exception {
@@ -79,5 +97,7 @@ public class CookingController implements Initializable {
 		return taskSequence;
 	}
 	*/
+	
+	
 	
 }
