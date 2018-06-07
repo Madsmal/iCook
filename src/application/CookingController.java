871fb114b0b0621c6b707a018908d73a816802ff
Madsmal <Madsmalmskov@hotmail.com>
2018-06-07 10:26:20 +0200

    Update CookingController.java
    Added LongestPath

--- src/application/CookingController.java
+++ src/application/CookingController.java
@@ -87,7 +87,7 @@ public class CookingController implements Initializable {
 		
 		//Algorithm algorithm = new Algorithm(); 
 		//algorithm.calculateTaskSequence();
-
+		//algorithm.longestPath();
 		
 		//TEMPORARY START
 						taskSequence[0] = 0;
@@ -317,7 +317,7 @@ public class CookingController implements Initializable {
 		}
 	}
 
-	private void updateProgressBar() { //TODO attentionRequired false kun tælle med som det sidste
+	private void updateProgressBar() { //TODO attentionRequired false kun tÃ¦lle med som det sidste
 		timePassed = 0;
 		for (int i = 0 ; i < currentTask ; i++) {
 			timePassed = timePassed + (Model.recipe.tasks.task.get(i).getTime());
