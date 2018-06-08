7d610971978134312e92879966bedb07ce20e7d9
Mads Malmskov <Madsmalmskov@hotmail.com>
2018-06-08 17:15:04 +0200

    Minor adjustments / clean up in code

--- src/application/Algorithm.java
+++ src/application/Algorithm.java
@@ -56,40 +56,25 @@ public class Algorithm extends CookingController {
 			}		
 		}
 		System.out.println(sequence);
-		System.out.println(totalTime);
 		// Checks if element has a child and attReq is false. If that's the case then it should have a higher priority than other elements. 
 		for(int i = 0; i < Model.recipe.tasks.task.size(); i++) {
-			//			if (Model.recipe.tasks.task.get(i).attentionRequired == false && !ArrayUtils.contains(Model.recipe.tasks.task.get(i).children, "0")) {	
-			//				sequence.remove(i);
-			//				sequence.add(0, Model.recipe.tasks.task.get(i).ID);
-			//			}	
-			// Note: Not sure if this is necessary. Should the child to a parent be on the same index as before or should it be made right after the parent is done. 
-			// This statement moves the child to the index after the parent. 
+						if (Model.recipe.tasks.task.get(i).attentionRequired == false && !ArrayUtils.contains(Model.recipe.tasks.task.get(i).children, "0")) {	
+							sequence.remove(i);
+							sequence.add(0, Model.recipe.tasks.task.get(i).ID);
+							totalTime = totalTime - Model.recipe.tasks.task.get(i).time;
+						}	
+			// Note: this piece is not necessary. The child shouldn't move ahead in the array - this could be discussed but I decided not to.
 			//			 if(Model.recipe.tasks.task.get(i).attentionRequired == false && !ArrayUtils.contains(Model.recipe.tasks.task.get(i).parents, "0")) {
 			//				System.out.println(Model.recipe.tasks.task.get(i).ID);
 			//				sequence.remove(i);
 			//				sequence.add(0, Model.recipe.tasks.task.get(i).ID);	
 			//			}
 
-			//			if(!ArrayUtils.contains(Model.recipe.tasks.task.get(i).parents, "0")) {
-			//				System.out.println("id: " + Model.recipe.tasks.task.get(i).ID);
-			//
-			//				System.out.println("Parent: " + Model.recipe.tasks.task.get(i-1).ID);
-			//					// Case: if i+test = last index
-			//					if(i+1 == Model.recipe.tasks.task.size()) {
-			//						System.out.println("i = max value: " + Model.recipe.tasks.task.get(i).ID);
-			//
-			//						break;
-			//					}
-			//					else if(!ArrayUtils.contains(Model.recipe.tasks.task.get(i).children, "0")) {
-			//						System.out.println("ID: " + Model.recipe.tasks.task.get(i).ID);
-			//
-			//					}
-			//				}
-			//				if(ArrayUtils.contains(Model.recipe.tasks.task.get(i).children, i+1)) {
-			//					System.out.println("i: " + Model.recipe.tasks.task.get(i).ID);
 		}
-
+		if(totalTime < 0) {
+			System.out.println("Will fix later ");
+		}
+		System.out.println(totalTime);
 		// Stream converts List<integer> to int[].
 
 		int[] taskSequence = sequence.stream().mapToInt(i->i).toArray();
@@ -124,7 +109,26 @@ public class Algorithm extends CookingController {
 			}
 			//System.out.println(tests);
 		}
-				
+		
+		//		if(!ArrayUtils.contains(Model.recipe.tasks.task.get(i).parents, "0")) {
+		//				System.out.println("id: " + Model.recipe.tasks.task.get(i).ID);
+		//
+		//				System.out.println("Parent: " + Model.recipe.tasks.task.get(i-1).ID);
+		//					// Case: if i+test = last index
+		//					if(i+1 == Model.recipe.tasks.task.size()) {
+		//						System.out.println("i = max value: " + Model.recipe.tasks.task.get(i).ID);
+		//
+		//						break;
+		//					}
+		//					else if(!ArrayUtils.contains(Model.recipe.tasks.task.get(i).children, "0")) {
+		//						System.out.println("ID: " + Model.recipe.tasks.task.get(i).ID);
+		//
+		//					}
+		//				}
+		//				if(ArrayUtils.contains(Model.recipe.tasks.task.get(i).children, i+1)) {
+		//					System.out.println("i: " + Model.recipe.tasks.task.get(i).ID);
+		
+		
 		int[] taskSequence = sequence.stream().mapToInt(i->i).toArray();
 		System.out.println(java.util.Arrays.toString(taskSequence));
 		return taskSequence;
