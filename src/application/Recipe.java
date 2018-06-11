71fb4e18c18a572161b4542d4d2f595182a8d9f9
Erik Nikolajsen <erik.nikolajsen@gmail.com>
2018-06-11 18:06:49 +0200

    Cleanup

--- src/application/Recipe.java
+++ src/application/Recipe.java
@@ -12,7 +12,6 @@ import javax.xml.bind.annotation.XmlMixed;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-//https://www.thoughts-on-java.org/generate-your-jaxb-classes-in-second/
 /**
  * <p>Java class for anonymous complex type.
  * 
@@ -927,73 +926,3 @@ public class Recipe {
 }
 
 
-	//class for recipes
-//	private int id;
-//	private String name;
-//	private int numberOfServes;
-//	private String ingredients;
-//	private String steps;
-//	private String notes;
-//	
-//	public Recipe(int id, String name, int numberOfServes, String ingredients) {
-//		this.id = id;
-//		this.name = name;
-//		this.numberOfServes = numberOfServes;
-//		this.ingredients = ingredients;
-//		//this.steps = steps;
-//		//this.notes = notes;
-//	}
-//	
-//	public int getId() {
-//		return id;
-//	}
-//	
-//	public String getName() {
-//		return name;
-//	}
-//	
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	
-//	public int getNumberOfServes() {
-//		return numberOfServes;
-//	}
-//	
-//	public void setNumberOfServes(int numberOfServes) {
-//		this.numberOfServes = numberOfServes;
-//	}
-//	
-//	public String getIngredients() {
-//		return ingredients;
-//	}
-//	
-//	public void setIngredients(String ingredients) {
-//		this.ingredients = ingredients;
-//	}
-//	
-//	public String getSteps() {
-//		return steps;
-//	}
-//	
-//	public void setSteps(String steps) {
-//		this.steps = steps;
-//	}
-//	
-//	public String getNotes() {
-//		return notes;
-//	}
-//	
-//	public void setNotes(String notes) {
-//		this.notes = notes;
-//	}
-//	
-//	public void updateFields(String name, int serves, String ingredients, String steps, String notes, int numberOfServes) {
-//		this.name = name;
-//		this.numberOfServes = numberOfServes;
-//		this.ingredients = ingredients;
-//		this.steps = steps;
-//		this.notes = notes;
-//	}
-//	
-//}
