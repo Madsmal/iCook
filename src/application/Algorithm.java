e80e6b400165014d9139cc1ed91d7886e78abeb0
Mads Malmskov <Madsmalmskov@hotmail.com>
2018-06-18 22:40:07 +0200

    No print statement

--- src/application/Algorithm.java
+++ src/application/Algorithm.java
@@ -147,7 +147,7 @@ public class Algorithm extends CookingController {
 		
 		// Stream converts List<integer> to int[].
 		taskSequence = sequence.stream().mapToInt(i->i).toArray();
-		System.out.println(java.util.Arrays.toString(taskSequence));
+		//System.out.println(java.util.Arrays.toString(taskSequence));
 
 		return taskSequence;
 	} 
